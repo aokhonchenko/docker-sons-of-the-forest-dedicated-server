bb763f13cda4525340bbffd2bdc3f15441df2085
aokhonchenko <aleksand.okhonchenko@x5.ru>
2024-07-10 09:19:51 +0300

    added my data

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker volume create steamcmd
 docker volume create sonsoftheforest
-docker run --rm -i -t -p 8766:8766/udp -p 27016:27016/udp -p 9700:9700/udp -v steamcmd:/steamcmd -v sonsoftheforest:/sonsoftheforest --name sons-of-the-forest-dedicated-server ohotnik/sotf-ds:latest
+docker run -i -t -p 8766:8766/udp -p 27016:27016/udp -p 9700:9700/udp -v steamcmd:/steamcmd -v sonsoftheforest:/sonsoftheforest --restart unless-stopped --name sons-of-the-forest-dedicated-server ohotnik/sotf-ds:latest
